bc3ed2fec71e7b0ddd0a216b4fb5dd930eea22ac
ArkanDash <suckgamer0@gmail.com>
2024-12-12 10:46:33 +0700

    fix: config

--- src/app/api/students/route.tsx
+++ src/app/api/students/route.tsx
@@ -1,7 +1,8 @@
 import { NextResponse } from "next/server"
-import characterData from "@/data/Characters.json"
 export const config = {}
 
 export async function GET() {
-    return NextResponse.json(characterData)
+    return NextResponse.json({
+        "data": "Unavailable at the moment"
+    })
 }
